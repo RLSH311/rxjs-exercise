ad8c49c731d0a5f9fd65a32c37b5025b20d3af27
RLSH <harel.shimon@hotmail.com>
2020-11-24 12:46:20 +0200

    finshed exercise1

--- src/exercises/exercise1.js
+++ src/exercises/exercise1.js
@@ -1,7 +1,8 @@
-const {fromHttpRequest} = require('../utils/http');
+const { fromHttpRequest } = require('../utils/http');
+const { map } = require('rxjs/operators');
 
 fromHttpRequest('https://orels-moviedb.herokuapp.com/directors')
     .pipe(
-        // add the proper operators to emit the correct information
+        map(directors => directors[0])
     )
     .subscribe(console.log);
