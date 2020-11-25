b9c02d297092491de2e385670f4693f85c56821c
RLSH <harel.shimon@hotmail.com>
2020-11-25 11:44:27 +0200

    now exercise 8 works

--- src/exercises/exercise8.js
+++ src/exercises/exercise8.js
@@ -1,23 +1,21 @@
 const { fromHttpRequest } = require('../utils/http');
-const { map, concatMap } = require('rxjs/operators');
+const { map, concatMap, tap } = require('rxjs/operators');
+const { concat } = require('rxjs');
 
-let directorsName$ = fromHttpRequest('https://orels-moviedb.herokuapp.com/directors')
-                        .pipe(
-                            concatMap(director => director),
-                            map(director => director.name)
-                        )
-                        .subscribe(console.log);
 
-let genresNames$ = fromHttpRequest('https://orels-moviedb.herokuapp.com/genres')
-                    .pipe(
-                        concatMap(genre => genre),
-                        map(genre => genre.name)
-                    )
-                    .subscribe(console.log);
+let genresNames$ = fromHttpRequest('https://orels-moviedb.herokuapp.com/genres').pipe(
+                    concatMap(genre => genre),
+                    map(genre => genre.name),
+                    tap(console.log));
+    
+let directorsName$ = fromHttpRequest('https://orels-moviedb.herokuapp.com/directors').pipe(
+                        concatMap(director => director),
+                        map(director => director.name),
+                        tap(console.log));
 
-fromHttpRequest('https://orels-moviedb.herokuapp.com/movies')
-    .pipe(
-        concatMap(movie => movie),
-        map(movie => movie.title)
-    )
-    .subscribe(console.log);
+let moviesTitles$ = fromHttpRequest('https://orels-moviedb.herokuapp.com/movies').pipe(
+                        concatMap(movie => movie),
+                        map(movie => movie.title),
+                        tap(console.log));
+
+concat(moviesTitles$, directorsName$, genresNames$).subscribe();
