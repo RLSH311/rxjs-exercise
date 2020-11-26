788035fef853648cd65ec476401b39db237c9b54
RLSH <harel.shimon@hotmail.com>
2020-11-26 11:07:01 +0200

    done exercise 11

--- src/exercises/exercise11.js
+++ src/exercises/exercise11.js
@@ -1,19 +1,44 @@
 const { fromHttpRequest } = require('../utils/http');
-const { concatMap, tap, mergeMap, filter, map, count, take } = require('rxjs/operators');
+const { map, concatMap, filter, count, mergeMap, tap, take, skip } = require('rxjs/operators');
 
-fromHttpRequest('https://orels-moviedb.herokuapp.com/ratings')
-    .pipe(
+let ratesBiggerThanValue = {};
+
+function findMovieRatesThatBiggerThanValue(movie, minRate) {
+    if (ratesBiggerThanValue[movie.title] === undefined) {
+        ratesBiggerThanValue[movie.title] = {ratesBiggerThanValue: 0, totalRates: 0};
+    }
+
+    return fromHttpRequest('https://orels-moviedb.herokuapp.com/ratings').pipe(
         concatMap(rate => rate),
-        take(1),
-        tap(rate => console.log(rate.movie)),
-        mergeMap(rateA => {
-            return fromHttpRequest('https://orels-moviedb.herokuapp.com/ratings')
-                    .pipe(
-                        concatMap(rateB => rateB),
-                        filter(rateB => rateB.movie === rateA.movie),
-                        count(sameMovieRate => sameMovieRate.score >= 1),
-                        tap(console.log)
-                    )
+        filter(rate => rate.movie === movie.id),
+        map(sameMovieRate => {
+            ratesBiggerThanValue[movie.title].totalRates += 1;
+            return sameMovieRate;
+        }),
+        filter(sameMovieRate => sameMovieRate.score >= minRate),
+        count(),
+        map(ratesBiggerThanValueAmount => {
+            ratesBiggerThanValue[movie.title].ratesBiggerThanValue += ratesBiggerThanValueAmount;
+            return ratesBiggerThanValue[movie.title];
         })
+    );
+}
+
+fromHttpRequest('https://orels-moviedb.herokuapp.com/movies')
+    .pipe(
+        concatMap(movie => movie),
+        // running only on few movies so the operation won't take too long
+        skip(75),
+        take(35),
+        mergeMap(movie => findMovieRatesThatBiggerThanValue(movie, 3).pipe(
+            filter(movieRating => {
+                return movieRating.ratesBiggerThanValue / movieRating.totalRates >= 0.7;
+            }),
+            tap(highRatedMovie => {
+                let precentage = (highRatedMovie.ratesBiggerThanValue / highRatedMovie.totalRates) * 100;
+                console.log(`the movie '${movie.title}' have ${precentage}% rates >= 3`);
+                return precentage;
+            })
+        ))
     )
     .subscribe();
